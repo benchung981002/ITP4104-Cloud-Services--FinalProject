32b6c627ed016da12bc522851f31c25e887d0e18
benchung981002 <44958912+benchung981002@users.noreply.github.com>
2018-11-12 12:22:21 +0800

    Create deployment.sh

---
+++ deployment.sh
@@ -0,0 +1,11 @@
+echo "Deploy $STACK_NAME stack"
+
+sourcebucket=my$STACK_NAME
+aws s3 mb s3://$sourcebucket --region $REGION
+rm package.yaml
+sam package --template-file template.yaml --s3-bucket $sourcebucket --output-template-file package.yaml
+
+aws cloudformation deploy --stack-name $STACK_NAME --template-file package.yaml \
+--region $REGION --capabilities CAPABILITY_IAM \
+--parameter-overrides \
+    RunUnitTest="true"
